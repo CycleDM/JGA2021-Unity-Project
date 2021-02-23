9fdd40346a56787296bb6379d1325733c2c54e4d
CycleDM <cycledm0508@gmail.com>
2021-02-23 19:04:22 +0900

    少し変更

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,39 +6,45 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rig;
     public float moveSpeed = 5;
-    public float jumpVelocity = 5;
+    //public float jumpVelocity = 5;
+    //private bool isOnGround = true;
 
-    private bool isOnGround = true;
+    public Transform rotationTarget;
+    public float rotationRadius;
+
+    private float angle;
+
+    private Vector3 moveDir;
 
     // Start is called before the first frame update
     private void Start()
     {
         rig = GetComponent<Rigidbody>();
+
+        moveDir = Vector3.right;
     }
 
     // Update is called once per frame
     private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
 
-        rig.transform.Translate(Vector3.forward * vertical * moveSpeed * Time.deltaTime);
-        rig.transform.Translate(Vector3.right * horizontal * moveSpeed * Time.deltaTime);
-
+        //rig.transform.Translate(Vector3.forward * horizontal * moveSpeed * Time.deltaTime);
+        
         // Jump
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (isOnGround)
-            {
-                rig.velocity += new Vector3(0, jumpVelocity, 0);
-                //rig.AddForce(Vector3.up * 300);
-                isOnGround = false;
-            }
-        }
+        //if (Input.GetButtonDown("Jump"))
+        //{
+        //    if (isOnGround)
+        //    {
+        //        rig.velocity += new Vector3(0, jumpVelocity, 0);
+        //        //rig.AddForce(Vector3.up * 300);
+        //        isOnGround = false;
+        //    }
+        //}
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        isOnGround = true;
+        //isOnGround = true;
     }
 }
