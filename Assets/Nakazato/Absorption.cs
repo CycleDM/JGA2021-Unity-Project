a8b35817234b011dd146c7f57085db2eaf218282
aki-main <71026012+const-aki@users.noreply.github.com>
2021-02-24 18:38:00 +0900

    test:ガラクタ吸引処理とカメラ制御

---
+++ Assets/Nakazato/Absorption.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Absorption : MonoBehaviour
+{
+    // プレイヤークラスの取得
+    private PlayerController playerController;
+    [SerializeField] private GameObject playerObj;
+    [SerializeField] private Vector3 playerPos;
+    [SerializeField] private Vector3 junkPos;
+
+    // 吸収速度
+    [SerializeField] private float moveVelocity = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // プレイヤーのタグ取得
+        playerObj = GameObject.FindWithTag("Player");
+        playerController = playerObj.GetComponent<PlayerController>();
+
+        playerPos = playerObj.transform.position;
+        junkPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // true
+        if(playerController.GetAbsorption())
+        {
+            playerPos = playerObj.transform.position;
+            junkPos = transform.position;
+    
+            junkPos.x += (playerPos.x - junkPos.x) * moveVelocity;
+            junkPos.y += (playerPos.y - junkPos.y) * moveVelocity;
+            junkPos.z += (playerPos.z - junkPos.z) * moveVelocity;
+            transform.position = junkPos;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,10 +10,15 @@ public class PlayerController : MonoBehaviour
 
     private bool isOnGround = true;
 
+    private bool isAbsorption = false; // 吸収
+    private Vector3 playerPos; //プレイヤーのポジション
+
     // Start is called before the first frame update
     private void Start()
     {
         rig = GetComponent<Rigidbody>();
+        playerPos = GetComponent<Transform>().position;
+        isAbsorption = false;
     }
 
     // Update is called once per frame
@@ -25,6 +30,17 @@ public class PlayerController : MonoBehaviour
         rig.transform.Translate(Vector3.forward * vertical * moveSpeed * Time.deltaTime);
         rig.transform.Translate(Vector3.right * horizontal * moveSpeed * Time.deltaTime);
 
+        rig.velocity = new Vector3(horizontal * moveSpeed, 0, vertical * moveSpeed); //プレイヤーのRigidbodyに対してInputにspeedを掛けた値で更新し移動
+
+        Vector3 diff = transform.position - playerPos; //プレイヤーがどの方向に進んでいるかがわかるように、初期位置と現在地の座標差分を取得
+
+        if (diff.magnitude > 0.01f)
+        {
+            transform.rotation = Quaternion.LookRotation(diff);  //ベクトルの情報をQuaternion.LookRotationに引き渡し回転量を取得しプレイヤーを回転させる
+        }
+
+        playerPos = transform.position; //プレイヤーの位置を更新
+
         // Jump
         if (Input.GetButtonDown("Jump"))
         {
@@ -35,10 +51,27 @@ public class PlayerController : MonoBehaviour
                 isOnGround = false;
             }
         }
+
+        // 吸い込み
+        if (Input.GetKey(KeyCode.R))
+        {
+            Debug.Log("Press.R true");
+            isAbsorption = true;
+        }
+        else
+        {
+            Debug.Log("Press.R false");
+            isAbsorption = false;
+        }
     }
 
     private void OnCollisionEnter(Collision other)
     {
         isOnGround = true;
+    }
+
+    public bool GetAbsorption()
+    {
+        return isAbsorption;
     }
 }
