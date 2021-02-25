a8731a9b6c386f7c8d9fba052f24a2be4d438c5e
aki-main <71026012+const-aki@users.noreply.github.com>
2021-02-25 12:37:29 +0900

    吸い込み判定処理調整

--- Assets/Nakazato/Absorption.cs
+++ Assets/Nakazato/Absorption.cs
@@ -13,8 +13,10 @@ public class Absorption : MonoBehaviour
     // 吸収速度
     [SerializeField] private float moveVelocity = 0.005f;
 
-    private Cleaner cleaner;
     [SerializeField] private GameObject cleanerObj;
+
+
+    [SerializeField] private bool isActive = false;
 
     // Start is called before the first frame update
     void Start()
@@ -27,65 +29,80 @@ public class Absorption : MonoBehaviour
         junkPos = transform.position;
 
         cleanerObj = GameObject.FindWithTag("Cleaner");
-        cleaner = cleanerObj.GetComponent<Cleaner>();
+
+        isActive = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         // true
-        if(playerController.GetAbsorption() && cleaner.GetActive())
+        if(playerController.GetAbsorption() && isActive)
         {
-            //playerPos = playerObj.transform.position;
-            //junkPos = transform.position;
-    
-            //if (playerPos.x > junkPos.x)
-            //{
-            //    junkPos.x = junkPos.x + moveVelocity;
-            //}
-            //else if (playerPos.x < junkPos.x)
-            //{
-            //    junkPos.x = junkPos.x - moveVelocity;
-            //}
-    
-            //if (playerPos.y > junkPos.y)
-            //{
-            //    junkPos.y = junkPos.y + moveVelocity;
-            //}
-            //else if (playerPos.y < junkPos.y)
-            //{
-            //    junkPos.y = junkPos.y - moveVelocity;
-            //}
-
-            //if (playerPos.z > junkPos.z)
-            //{
-            //    junkPos.z = junkPos.z + moveVelocity;
-            //}
-            //else if (playerPos.z < junkPos.z)
-            //{
-            //    junkPos.z = junkPos.z - moveVelocity;
-            //}
-    
-            //transform.position = junkPos;
-
-
             playerPos = playerObj.transform.position;
             junkPos = transform.position;
     
-            junkPos.x += (playerPos.x - junkPos.x) * moveVelocity;
-            junkPos.y += (playerPos.y - junkPos.y) * moveVelocity;
-            junkPos.z += (playerPos.z - junkPos.z) * moveVelocity;
+            if (playerPos.x > junkPos.x)
+            {
+                junkPos.x = junkPos.x + moveVelocity;
+            }
+            else if (playerPos.x < junkPos.x)
+            {
+                junkPos.x = junkPos.x - moveVelocity;
+            }
+    
+            if (playerPos.y > junkPos.y)
+            {
+                junkPos.y = junkPos.y + moveVelocity;
+            }
+            else if (playerPos.y < junkPos.y)
+            {
+                junkPos.y = junkPos.y - moveVelocity;
+            }
+
+            if (playerPos.z > junkPos.z)
+            {
+                junkPos.z = junkPos.z + moveVelocity;
+            }
+            else if (playerPos.z < junkPos.z)
+            {
+                junkPos.z = junkPos.z - moveVelocity;
+            }
+    
             transform.position = junkPos;
+
+
+           // playerPos = playerObj.transform.position;
+           // junkPos = transform.position;
+    //
+           // junkPos.x += (playerPos.x - junkPos.x) * moveVelocity;
+           // junkPos.y += (playerPos.y - junkPos.y) * moveVelocity;
+           // junkPos.z += (playerPos.z - junkPos.z) * moveVelocity;
+           // transform.position = junkPos;
         }
     }
 
     // 吸い込み後の処理 
-    private void OnCollisionEnter(Collision col)
+    void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.tag == "Player" && playerController.GetAbsorption())
+        if (col.gameObject.CompareTag("Player") && playerController.GetAbsorption())
         { // ガラクタを破壊
+            Debug.Log("Destroy : cube");
             Destroy(this.gameObject);
         }
     }
-    
+
+    void OnTriggerStay(Collider col)
+    {
+        if(col.gameObject.CompareTag("Cleaner"))
+        {
+            Debug.Log("Cleaner hit");
+            isActive = true;
+        }
+        else
+        {
+            Debug.Log("Cleaner not hit");
+            isActive = false;
+        }
+    }
 }
