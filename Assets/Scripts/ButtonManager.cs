a149a64293fef17331c5117308f9d1b5da8d1ce1
kouki-fukuda <73696244+kouki-fukuda@users.noreply.github.com>
2021-04-26 07:35:23 +0900

    シーン遷移修正

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -6,6 +6,9 @@ public class ButtonManager : MonoBehaviour
 {
 
 public SceneChanger SceneChanger;
+public GameObject Floor;
+float time;
+bool startFlag = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +19,23 @@ public SceneChanger SceneChanger;
     // Update is called once per frame
     public void OnclickStart()
     {
-        SceneChanger.SetSceneChange(true);
+        time = Time.time;
+        Destroy(Floor);
+        startFlag = true;
     }
     public void OnclickQuit()
     {
         UnityEngine.Application.Quit();
            UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    void Update()
+    {
+        if(startFlag)
+        if(time + 3f < Time.time)
+        {
+        SceneChanger.SetSceneChange(true);
+        startFlag = false;
+        }
     }
 }
