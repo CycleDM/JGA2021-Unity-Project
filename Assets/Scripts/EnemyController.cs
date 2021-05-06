e7f9834449e5e2e30b4cf8b31e31c3e7a8ea3d03
Soap <78839452+xylh1116@users.noreply.github.com>
2021-05-06 10:05:27 +0900

    Revert "犬のAIが完成した"
    This reverts commit d250b9352a3fa7cb4c3c05d71257c43218968758.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,53 +7,24 @@ public class EnemyController : MonoBehaviour
     private NavMeshAgent Enemy;//NavMeshAgentをget
     public GameObject Target;//目的地
     // Start is called before the first frame update
-    public float FindDis;//探す範囲
-    public Transform[] directPoints;//移動範囲設定する点
-    private int index = 0;//循環用記録変数
-    public float stopTime = 3f;//停止時間
-    private float timer = 0;
-    private float a;
-    void Awake()
+     public float FindDis;//探す範囲
+
+    void Start()
     {
-        //Enemy = gameObject.GetComponent<NavMeshAgent>();
-        Enemy = GetComponent<NavMeshAgent>();
-        Enemy.destination = directPoints[index].position;
+        Enemy = gameObject.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float dis = Vector3.Distance(transform.position,Target.transform.position);//enemyと目的地の距離
-
-
-    if (Enemy.remainingDistance < 2.0f)
-    {
-
-
-        timer += Time.deltaTime;
-
-        if(timer >= stopTime)
+        if(Target != null)//目的地が存在する場合
         {
-            Debug.Log("123");
-            index++;
-            index %= 4;
-            timer = 0;
-            Enemy.destination = directPoints[index].position;
-
+            Enemy.destination = Target.transform.position;//目的地へ移動する
         }
-    }
-
         if(dis <= FindDis)//目的地は探す範囲内
         {
-            if(gameObject.tag == "Dog")
-            {
-                if(Target != null)//目的地が存在する場合
-                {
-                    Enemy.destination = Target.transform.position;//目的地へ移動する
-                }
-
-            }
-
+            Enemy.Stop();//移動停止
         }
         if(dis >= FindDis)
         {
